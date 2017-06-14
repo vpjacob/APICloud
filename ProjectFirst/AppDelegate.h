cc2aad01c3dddfe36592cbf705d7a52de6a37c97
vpjacob <824629419@qq.com>
2017-06-14 16:22:36 +0800

    提交代码

---
+++ ProjectFirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectFirst
+//
+//  Created by kenny on 16/1/7.
+//  Copyright (c) 2016年 APICloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
